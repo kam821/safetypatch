3ec6fb39b8add4b694ae2310d634d2be71650b01
penn5 <penn5@users.noreply.github.com>
2018-06-28 09:59:27 +0100

    Update config.sh
    to disable file patching

--- config.sh
+++ config.sh
@@ -22,13 +22,13 @@
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
-AUTOMOUNT=true
+AUTOMOUNT=false
 
 # Set to true if you need to load system.prop
 PROPFILE=false
 
 # Set to true if you need post-fs-data script
-POSTFSDATA=false
+POSTFSDATA=true
 
 # Set to true if you need late_start service script
 LATESTARTSERVICE=false
@@ -40,9 +40,9 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
-  ui_print "*******************************"
+  ui_print "********************************"
+  ui_print "* SafetyPatcher by hackintosh5 *"
+  ui_print "********************************"
 }
 
 ##########################################################################################
@@ -52,18 +52,9 @@ print_modname() {
 # List all directories you want to directly replace in the system
 # Check the documentations for more info about how Magic Mount works, and why you need this
 
-# This is an example
-REPLACE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
-
 # Construct your own list here, it will override the example above
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
-REPLACE="
-"
+REPLACE=""
 
 ##########################################################################################
 # Permissions
