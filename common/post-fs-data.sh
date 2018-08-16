b71e4f8de03737d9bbba119c9f407ac87d946383
penn5 <penn5@users.noreply.github.com>
2018-08-16 19:20:51 +0100

    Update post-fs-data.sh
    Fix for some devices like OP3

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -5,5 +5,5 @@ MODDIR=${0%/*}
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
-sed 's/ORANGE/GREEN/' /proc/cmdline > /data/local/tmp/cmdline
+sed 's/ORANGE/GREEN/i' /proc/cmdline > /data/local/tmp/cmdline
 mount -o bind /data/local/tmp/cmdline /proc/cmdline
