673cfb68520841f5972d186edb9376628dbcca81
penn5 <penn5@users.noreply.github.com>
2018-10-27 11:03:57 +0100

    Update post-fs-data.sh

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -7,5 +7,8 @@ MODDIR=${0%/*}
 # More info in the main Magisk thread
 sed 's/ORANGE/GREEN/i' /proc/cmdline | sed 's/YELLOW/GREEN/i' > /data/local/tmp/cmdline
 mount -o bind /data/local/tmp/cmdline /proc/cmdline
+
 sed 's;^ro.build.fingerprint=.*;ro.build.fingerprint=HUAWEI/CLT-L29/HWCLT-L29/128(C432):user/release-keys;' /system/build.prop > /data/local/tmp/build.prop
 mount -o bind /data/local/tmp/build.prop /system/build.prop
+resetprop ro.build.fingerprint HUAWEI/CLT-L29/HWCLT-L29/128(C432):user/release-keys
+# The build.prop thing seems to be useless, but at some point Google are sure to catch on and check in the raw build.prop to see if we're abusing getprop/resetprop.
