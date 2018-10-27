5ff460701efd83b0ee1f33b2e13c96a3b49f6966
Hackintosh5 <penn.mackintosh@gmail.com>
2018-10-27 15:24:39 +0100

    Finish new features and bump version

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -8,7 +8,10 @@ MODDIR=${0%/*}
 sed 's/ORANGE/GREEN/i' /proc/cmdline | sed 's/YELLOW/GREEN/i' > /data/local/tmp/cmdline
 mount -o bind /data/local/tmp/cmdline /proc/cmdline
 
-sed 's;^ro.build.fingerprint=.*;ro.build.fingerprint=HUAWEI/CLT-L29/HWCLT-L29/128(C432):user/release-keys;' /system/build.prop > /data/local/tmp/build.prop
+sed 's;^ro.*\.build\.fingerprint=.*;ro.build.fingerprint=HUAWEI/CLT-L29/HWCLT:8.1.0/HUAWEICLT-L29/128(C432):user/release-keys;' /system/build.prop > /data/local/tmp/build.prop
 mount -o bind /data/local/tmp/build.prop /system/build.prop
-resetprop ro.build.fingerprint HUAWEI/CLT-L29/HWCLT-L29/128(C432):user/release-keys
 # The build.prop thing seems to be useless, but at some point Google are sure to catch on and check in the raw build.prop to see if we're abusing getprop/resetprop.
+resetprop ro.build.fingerprint 'HUAWEI/CLT-L29/HWCLT:8.1.0/HUAWEICLT-L29/128(C432):user/release-keys'
+resetprop ro.bootimage.build.fingerprint 'HUAWEI/CLT-L29/HWCLT:8.1.0/HUAWEICLT-L29/128(C432):user/release-keys'
+#resetprop ro.vendor.build.fingerprint 'HUAWEI/CLT-L29/HWCLT:8.1.0/HUAWEICLT-L29/128(C432):user/release-keys'
+#The above caused issues (critical services not starting) on my Honor
